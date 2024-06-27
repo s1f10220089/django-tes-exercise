5a2fd75adfa55b5b4c0eb51796a58d08fccfa14f
yamaguchi <s1f102200895@iniad.org>
2024-06-27 14:15:53 +0900

    ソート機能を追加

--- todo/views.py
+++ todo/views.py
@@ -2,14 +2,18 @@ from django.shortcuts import render
 from django.utils.timezone import make_aware
 from django.utils.dateparse import parse_datetime 
 from todo.models import Task 
-def index(request): 
-    if request.method == 'POST': 
+
+def index(request):
+    if request.method == 'POST':
         task = Task(title=request.POST['title'],
-                     due_at=make_aware(parse_datetime(request.POST['due_at']))) 
-        task.save() 
-    tasks = Task.objects.all() 
-    
-    context = { 
-        'tasks': tasks 
-    } 
+                    due_at=make_aware(parse_datetime(request.POST['due_at'])))
+        task.save()
+    if request.GET.get('order') == 'due':
+        tasks = Task.objects.order_by('due_at')
+    else:
+        tasks = Task.objects.order_by('-posted_at')
+
+    context = {
+            'tasks': tasks
+    }
     return render(request, 'todo/index.html', context)
